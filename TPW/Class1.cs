d2299095af24069be4c72f8ba77f0efc562cd476
kkomorowski236562 <komorowski.kacper.18@gmail.com>
2022-03-24 21:23:04 +0100

    Dodaj pliki projektów.

---
+++ TPW/Class1.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+
+namespace Hello
+{
+    public class Hello
+    {
+        private string hello;
+        public void set_hello(string x)
+        {
+            hello = x;
+        }
+        public string get_hello()
+        {
+            return hello;
+        }
+        public static void Main()
+        {
+            System.Console.WriteLine("Hello World!");
+        }
+    }
+}
