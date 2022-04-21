810e3c8f1d46d07ff504584f03480553f01ee17a
kkomorowski236562 <komorowski.kacper.18@gmail.com>
2022-04-21 18:17:53 +0200

    Szkic warstwy logicznej i danych, bez testow, do analizy

---
+++ Dane/dane.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dane
+{
+    public abstract class dane
+    {
+        private int Id { get; set; }
+        private int x;
+        private int y;
+        public void stworz_kulke()
+        {
+            Random rnd = new Random();
+            x = rnd.Next();
+            y = rnd.Next();
+        }
+        public void ustal_wspolrzedne(int x, int y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+    }
+}
+
---
+++ Logika/logika.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Dane;
+
+namespace Logika
+{
+    public abstract class logika
+    {
+        private int ruch_x;
+        private int ruch_y;
+        public void pokaz_wspolrzedne()
+        {
+            Console.WriteLine(x, ", ", y);
+        }
+        public void ruch_kulki()
+        {
+            Random rnd = new Random();
+            ruch_x = rnd.Next();
+            rnd = new Random();
+            ruch_y = rnd.Next();
+            switch (ruch_x)
+            {
+                case 0:
+                    x++;
+                    break;
+                case 1:
+                    x--;
+                    break;
+                default:
+                    x = x;
+                    break;
+            }
+            switch (ruch_y)
+            {
+                case 0:
+                    y++;
+                    break;
+                case 1:
+                    y--;
+                    break;
+                default:
+                    y = y;
+                    break;
+            }
+
+        }
+    }
+}
