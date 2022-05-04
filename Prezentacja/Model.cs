8e88c894ebc0e8d87fa96228fb79392ae9090646
Kamil <236552@edu.p.lodz.pl>
2022-05-04 16:04:48 +0200

    jak main???

--- Prezentacja/Model.cs
+++ Prezentacja/Model.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Logika;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,16 @@ using System.Threading.Tasks;
 
 namespace Prezentacja
 {
-    internal class Model
+    public class Main
     {
+        private readonly KulkiApi kule;
+        
+        internal Main(int width, int height)
+        {
+            width = kule.getP().X + 100;
+            height = kule.getP().Y + 100;
+            kule = KulkiApi.stworz_kulke();
+        }
     }
+
 }
--- Prezentacja/View.xaml.cs
+++ Prezentacja/View.xaml.cs
@@ -15,9 +15,6 @@ using System.Windows.Shapes;
 
 namespace Prezentacja
 {
-    /// <summary>
-    /// Logika interakcji dla klasy MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
         public MainWindow()
